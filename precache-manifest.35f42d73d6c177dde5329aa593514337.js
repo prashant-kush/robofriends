cf28fece1a6d64c2dfebd61715da3114b9fe3009
Prashant <prashantk.tps@gmail.com>
2019-04-21 00:01:17 +0530

    Updates

---
+++ precache-manifest.35f42d73d6c177dde5329aa593514337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "2a460063186db1dbe0e5",
+    "url": "/robofriends/static/js/main.2a460063.chunk.js"
+  },
+  {
+    "revision": "34c134bdda646c2902f9",
+    "url": "/robofriends/static/js/1.34c134bd.chunk.js"
+  },
+  {
+    "revision": "2a460063186db1dbe0e5",
+    "url": "/robofriends/static/css/main.10ecec7d.chunk.css"
+  },
+  {
+    "revision": "34c134bdda646c2902f9",
+    "url": "/robofriends/static/css/1.6661f1b5.chunk.css"
+  },
+  {
+    "revision": "eba7059567d0eed6fa7f330422ada3fb",
+    "url": "/robofriends/index.html"
+  }
+];
